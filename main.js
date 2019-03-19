369e3e1fce5a76b506e4842183f7a104b27a9e6d
Jameka Echols <jameka.echols@gmail.com>
2019-03-19 11:50:49 -0500

    finished lightening 2

--- main.js
+++ main.js
@@ -10,7 +10,26 @@ const doctor = (name, specialty, address) => ({
     "name": name,
     "specialty": specialty,
     "address": address
-})
+});
 
 const Ted = doctor("Theodore", "Obstrician", "4300 Brush Hill");
-console.log(Ted)
+console.log(Ted);
+
+/*
+Lightning Exercise 2: Write a factory function that creates an object that represents a pet. The function should accept two arguments.
+
+Pet name
+Pet breed
+Invoke the factory function 3 times and place each animal in an array stored in a variable named BowWowKennels
+*/
+const BowWowKennels = [];
+
+const pet = (name, breed) => ({
+    "name": name,
+    "breed": breed
+});
+
+BowWowKennels.push(pet("PorkChop", "Goldendoodle"));
+BowWowKennels.push(pet("Millie", "Poodle"));
+BowWowKennels.push(pet("Lasie", "Golden Retriever"));
+console.log(BowWowKennels);
